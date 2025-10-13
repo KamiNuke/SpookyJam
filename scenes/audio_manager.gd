020dc988420b5b477a39c23fb42d3b7cb3ff516a
Milo Curran <88947239+milocurran@users.noreply.github.com>
2025-10-13 03:01:28 +0100

    Menu Music, UI sounds, Volume Slider
    Do not merge yet - explored different options to implement audio, menu sounds easier to do without FMOD. Volume settings currently do not save once leaving settings pages

---
+++ scenes/audio_manager.gd
@@ -0,0 +1,39 @@
+extends Node
+
+@onready var music_player = $MusicPlayer
+@onready var sfx_button = $SFXButton
+@onready var sfx_play = $SFXPlay
+
+# ----------------
+# MUSIC
+# ----------------
+func play_music(fade_time := 1.0):
+	if music_player.stream == null:
+		push_warning("No stream assigned to MusicPlayer.")
+		return
+	music_player.volume_db = -30
+	music_player.play()
+	var tween = get_tree().create_tween()
+	tween.tween_property(music_player, "volume_db", 0, fade_time)
+
+func stop_music(fade_time := 1.0):
+	var tween = get_tree().create_tween()
+	tween.tween_property(music_player, "volume_db", -30, fade_time)
+	tween.tween_callback(Callable(music_player, "stop"))
+
+# ----------------
+# SFX
+# ----------------
+func play_sfx_button(volume_db := 0):
+	if sfx_button.stream == null:
+		push_warning("No stream assigned to SFXButton.")
+		return
+	sfx_button.volume_db = volume_db
+	sfx_button.play()
+
+func play_sfx_play(volume_db := 0, fade_time := 1.0):
+	if sfx_play.stream == null:
+		push_warning("No stream assigned to SFXPlay.")
+		return
+	sfx_play.volume_db = volume_db
+	sfx_play.play()
