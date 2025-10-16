546400d8f69326c7bcaa7bc18f2d6972646233bc
Milo Curran <88947239+milocurran@users.noreply.github.com>
2025-10-16 21:13:59 +0100

    Commit 1 Blocked out ambience beds

--- scenes/sound/EntranceAmbientPlayer.gd
+++ scenes/sound/EntranceAmbientPlayer.gd
@@ -1,12 +1,12 @@
 extends AudioStreamPlayer
 
-@export var fade_in_time: float = 0.6
-@export var fade_out_time: float = 4
+@export var fade_in_time: float = 1.5
+@export var fade_out_time: float = 1.5
 
 var tween: Tween = null
 
 func _ready():
-	volume_db = -60  # start silent
+	volume_db = -50  # start silent
 	stop()  # ensure player is not playing initially
 
 func fade_in():
