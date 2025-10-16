aa8aa9f3f86f5a04dc6e6797c6bf6a332fcd60a8
KamiNuke <150977125+KamiNuke@users.noreply.github.com>
2025-10-16 21:11:04 +0300

    spawn camera where it's needed

--- scenes/utilities/interact_raycast.gd
+++ scenes/utilities/interact_raycast.gd
@@ -1,6 +1,7 @@
 extends RayCast3D
 
 @onready var interact_crosshair: CenterContainer = $"../../../InteractCrosshair"
+const PLACED_CAMERA = preload("uid://bbq76bxa5xt65")
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -11,8 +12,16 @@ func _ready() -> void:
 func _process(delta: float) -> void:
 	if is_colliding():
 		interact_crosshair.visible = true
-		if Input.is_action_just_released("action") and !global.is_dialogue_active:
-			if get_collider().is_in_group("dialog"):
-				Dialogic.start(get_collider().dialogue)
+		if Input.is_action_just_released("action") and !global.is_dialogue_active and get_collider() != null:
+			var collider = get_collider()
+			if collider.is_in_group("dialog"):
+				Dialogic.start(collider.dialogue)
+			elif collider.is_in_group("camera"):
+				var pos = collider.position
+				collider.queue_free()
+				var cam = PLACED_CAMERA.instantiate()
+				cam.position = Vector3(pos.x, pos.y + 2.0, pos.z)
+				get_parent().get_parent().get_parent().get_parent().add_child(cam)
+				
 	else:
 		interact_crosshair.visible = false
